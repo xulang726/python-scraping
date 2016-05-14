1451f43823ad8c432abcd33e507f39cf0e544fd4
xulang726 <xulang726@gmail.com>
2016-05-14 22:51:58 +0800

    python3.5.1
    Warning (from warnings module):
    File "D:\Python35\lib\site-packages\bs4\__init__.py", line 166
    markup_type=markup_type))
    UserWarning: No parser was explicitly specified, so I'm using the best
    available HTML parser for this system ("html.parser"). This usually
    isn't a problem, but if you run this code on another system, or in a
    different virtual environment, it may use a different parser and behave
    differently.
    
    To get rid of this warning, change this:
    
    BeautifulSoup([your markup])
    
    to this:
    
    BeautifulSoup([your markup], "html.parser")
    Chap9-1 9-2

--- chapter2/3-findDescendants.py
+++ chapter2/3-findDescendants.py
@@ -1,7 +1,7 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-html = urlopen("http://www.pythonscraping.com/pages/page3.html")
+html = urlopen("http://www.pythonscraping.com/pages/page3.html","html.parser")
 bsObj = BeautifulSoup(html)
 
 for child in bsObj.find("table",{"id":"giftList"}).children:
